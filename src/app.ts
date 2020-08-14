e8daac46a07907bca7220e4bbde2770dcc4f3919
Julien CLAUDEL <jclaudel@enovacom.fr>
2020-08-14 17:33:38 +0200

    Added environment variable for PORT and TOKEN

--- src/app.ts
+++ src/app.ts
@@ -29,4 +29,4 @@ app.use("/errors", errorsRouter);
 app.use(errorsController);
 
 // Start express app on port 3000
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 import { CustomError } from "../utils/interfaces";
 
 export const authController: RequestHandler = (req, res, next) => {
-    if (req.headers.token === "magictoken") next();
+    if (req.headers.token === process.env.TOKEN) next();
     else {
         // Invalid token, reject request
         const invalidTokenError: CustomError = {
