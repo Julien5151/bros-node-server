043c891344f5f212c225db3c5765bc9cc1ef1d9a
Julien CLAUDEL <jclaudel@enovacom.fr>
2020-08-14 16:01:50 +0200

    Removed unused errors array

--- src/controllers/errors.ts
+++ src/controllers/errors.ts
@@ -1,8 +1,6 @@
 import { RequestHandler } from "express";
 import { CustomError } from "../utils/interfaces";
 
-const errors = [1, 2, 3, 4];
-
 export const errorsRouteController: RequestHandler = (req, res, next) => {
     const customError: CustomError = {
         statusCode: req.body.status,
