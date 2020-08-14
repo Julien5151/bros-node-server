6e916e92b23b9520deb8ca57ebe15a3e3a5f1ff2
Julien Claudel <jclaudel@enovacom.fr>
2020-08-14 12:11:09 +0200

    Added nodemon setup for dev

--- src/routes/errors.ts
+++ src/routes/errors.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-const errors = [1, 2, 3];
+const errors = [1, 2, 3, 4];
 
 router.get("/", (req, res, next) => {
     res.status(200).json({ errors: errors });
