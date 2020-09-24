33f9a5d28fbbe60f290b64d25eaab1b43aff09a0
Julien CLAUDEL <jclaudel@enovacom.fr>
2020-09-24 14:27:23 +0200

    Refactored signup controller to use async await

--- dist/auth/signup/signup.controllers.js
+++ dist/auth/signup/signup.controllers.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,7 +16,7 @@ exports.signupRouteController = void 0;
 const connectionPool_1 = require("../../utils/database/connectionPool");
 const enums_1 = require("../../utils/types/enums");
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
-exports.signupRouteController = (req, res, next) => {
+exports.signupRouteController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     // Extract data from body
     const reqBody = req.body;
     // Create new user object (visitor at signup)
@@ -16,20 +25,17 @@ exports.signupRouteController = (req, res, next) => {
         role: enums_1.UserRole.visitor,
         createdAt: new Date(),
     };
-    // Hash password using bcrypt
-    bcryptjs_1.default
-        .hash(reqBody.password, 12)
-        .then((hashedPassword) => {
-        return connectionPool_1.connectionPool.execute("INSERT INTO users (email, password, role, created_at) VALUES (?, ?, ?, ?);", [newUser.email, hashedPassword, newUser.role, newUser.createdAt]);
-    })
-        .then((mysqlResponse) => {
+    try {
+        // Hash password using bcrypt
+        const hashedPassword = yield bcryptjs_1.default.hash(reqBody.password, 12);
+        yield connectionPool_1.connectionPool.execute("INSERT INTO users (email, password, role, created_at) VALUES (?, ?, ?, ?);", [newUser.email, hashedPassword, newUser.role, newUser.createdAt]);
         // Create response object
         const response = {
             message: "Signup successfull",
         };
         return res.status(201).json(response);
-    })
-        .catch((err) => {
+    }
+    catch (err) {
         // Create custom error message from mysql error
         const error = {
             statusCode: 500,
@@ -37,5 +43,5 @@ exports.signupRouteController = (req, res, next) => {
         };
         // Pass error to error handler middleware
         next(error);
-    });
-};
+    }
+});
--- src/auth/signup/signup.controllers.ts
+++ src/auth/signup/signup.controllers.ts
@@ -1,5 +1,4 @@
 import { RequestHandler } from "express";
-import { MysqlError, OkPacket } from "mysql2";
 import { User } from "../../models/user";
 import { connectionPool } from "../../utils/database/connectionPool";
 import { UserRole } from "../../utils/types/enums";
@@ -7,7 +6,7 @@ import { CustomError } from "../../utils/types/interfaces";
 import { SignupRequest, SignupResponse } from "./signup.types";
 import bcrypt from "bcryptjs";
 
-export const signupRouteController: RequestHandler = (req, res, next) => {
+export const signupRouteController: RequestHandler = async (req, res, next) => {
     // Extract data from body
     const reqBody = req.body as SignupRequest;
     // Create new user object (visitor at signup)
@@ -16,29 +15,25 @@ export const signupRouteController: RequestHandler = (req, res, next) => {
         role: UserRole.visitor,
         createdAt: new Date(),
     };
-    // Hash password using bcrypt
-    bcrypt
-        .hash(reqBody.password, 12)
-        .then((hashedPassword) => {
-            return connectionPool.execute(
-                "INSERT INTO users (email, password, role, created_at) VALUES (?, ?, ?, ?);",
-                [newUser.email, hashedPassword, newUser.role, newUser.createdAt]
-            );
-        })
-        .then((mysqlResponse: OkPacket[] | any) => {
-            // Create response object
-            const response: SignupResponse = {
-                message: "Signup successfull",
-            };
-            return res.status(201).json(response);
-        })
-        .catch((err: MysqlError) => {
-            // Create custom error message from mysql error
-            const error: CustomError = {
-                statusCode: 500,
-                message: err.message,
-            };
-            // Pass error to error handler middleware
-            next(error);
-        });
+    try {
+        // Hash password using bcrypt
+        const hashedPassword = await bcrypt.hash(reqBody.password, 12);
+        await connectionPool.execute(
+            "INSERT INTO users (email, password, role, created_at) VALUES (?, ?, ?, ?);",
+            [newUser.email, hashedPassword, newUser.role, newUser.createdAt]
+        );
+        // Create response object
+        const response: SignupResponse = {
+            message: "Signup successfull",
+        };
+        return res.status(201).json(response);
+    } catch (err) {
+        // Create custom error message from mysql error
+        const error: CustomError = {
+            statusCode: 500,
+            message: err.message,
+        };
+        // Pass error to error handler middleware
+        next(error);
+    }
 };
