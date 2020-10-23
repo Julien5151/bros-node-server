0afa376a0359e05626609b10ba0effd1d7e15559
Julien CLAUDEL <jclaudel@enovacom.fr>
2020-10-23 09:43:39 +0200

    Added request for getting all users

--- dist/users/get/get-users.controller.js
+++ dist/users/get/get-users.controller.js
@@ -10,6 +10,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getUsersRouteController = void 0;
+const sql_queries_1 = require("../../utils/database/sql-queries");
 exports.getUsersRouteController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    return res.status(200).json({ message: "Hey !" });
+    // Select fields to extract
+    const fieldToExtract = [
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "address",
+        "zipcode",
+        "created_at",
+    ];
+    try {
+        // Extract rows from DB
+        const [rows] = yield sql_queries_1.SqlQueries.selectFrom("users", fieldToExtract);
+        // Cast and return the response
+        const response = rows;
+        return res.status(200).json(response);
+    }
+    catch (error) {
+        // In case of SQL error, log the error
+        console.error(error.message);
+        // Return a generic message to client
+        const customError = {
+            statusCode: 500,
+            message: "Something went wrong",
+        };
+        return next(customError);
+    }
 });
