1d5efe33d6bc0b9e5fc654f6ce59ca6553554517
Julien CLAUDEL <julienclaudel51@outlook.com>
2021-09-14 16:06:35 +0200

    Added CBOR decoding for attestation

--- dist/auth/signup/signup.controllers.js
+++ dist/auth/signup/signup.controllers.js
@@ -8,12 +8,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signupRouteController = void 0;
+const cbor_1 = __importDefault(require("cbor"));
 const signupRouteController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     // Extract data from body
     const reqBody = req.body;
     // Parse signup request
+    const clientDataJson = Buffer.from(reqBody.publicKeyCredential.response.clientDataJSON, "base64").toString();
+    const attestation = Buffer.from(reqBody.publicKeyCredential.response.attestationObject, "base64");
+    console.log(clientDataJson);
+    console.log(cbor_1.default.decodeFirstSync(attestation));
     // Create response object
     const response = {
         message: "Signup successfull",
--- src/auth/signup/signup.controllers.ts
+++ src/auth/signup/signup.controllers.ts
@@ -1,10 +1,23 @@
 import { RequestHandler } from "express";
 import { SignupRequest, SignupResponse } from "./signup.types";
+import cbor from "cbor";
 
 export const signupRouteController: RequestHandler = async (req, res, next) => {
     // Extract data from body
     const reqBody = req.body as SignupRequest;
     // Parse signup request
+    const clientDataJson = Buffer.from(
+        reqBody.publicKeyCredential.response.clientDataJSON as any,
+        "base64"
+    ).toString();
+
+    const attestation = Buffer.from(
+        (reqBody.publicKeyCredential.response as any).attestationObject,
+        "base64"
+    );
+
+    console.log(clientDataJson);
+    console.log(cbor.decodeFirstSync(attestation));
 
     // Create response object
     const response: SignupResponse = {
