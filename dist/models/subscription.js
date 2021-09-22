1a6f87259180dda5ab869137f09950a27758719b
Julien CLAUDEL <julienclaudel51@outlook.com>
2021-09-22 14:18:02 +0200

    Added methods to subcription model

--- dist/models/subscription.js
+++ dist/models/subscription.js
@@ -23,11 +23,91 @@ class Subscription {
         this.createdAt = (_b = subscriptionObject.createdAt) !== null && _b !== void 0 ? _b : new Date();
     }
     /**
+     * Loads subscription from DB using its _id. The same can be achieved by passing all
+     * arguments to constructor
+     */
+    static load(identifier) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Using an _id
+            const subscriptionData = yield db_connection_1.db
+                .collection(enums_1.MongoCollection.subscriptions)
+                .findOne({ _id: { $eq: identifier } });
+            // If a subscription is found, instanciate it and return the subscription
+            if (subscriptionData) {
+                return new this(subscriptionData);
+            }
+            else {
+                // Throw not found error
+                const notFoundError = {
+                    statusCode: 404,
+                    message: "Subscription not found",
+                };
+                throw notFoundError;
+            }
+        });
+    }
+    /**
+     * Loads all subscriptions from DB
+     */
+    static loadAll() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Using and email
+            const subscriptionsData = yield db_connection_1.db
+                .collection(enums_1.MongoCollection.subscriptions)
+                .find()
+                .toArray();
+            // If subscriptions are found, instanciate them and return the array
+            if (subscriptionsData.length > 0) {
+                const subscriptions = subscriptionsData.map((subcriptionData) => new this(subcriptionData));
+                return subscriptions;
+            }
+            else {
+                // Throw not found error
+                const notFoundError = {
+                    statusCode: 404,
+                    message: "No subscriptions found",
+                };
+                throw notFoundError;
+            }
+        });
+    }
+    /**
+     * Delete subscription from DB using its _id.
+     */
+    static delete(subscriptionId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Delete user from DB
+            return db_connection_1.db
+                .collection(enums_1.MongoCollection.subscriptions)
+                .deleteOne({ _id: { $eq: subscriptionId } });
+        });
+    }
+    /**
+     * WARNING : Delete ALL user documents
+     */
+    static deleteAll() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Delete all user documents from DB
+            return db_connection_1.db.collection(enums_1.MongoCollection.subscriptions).deleteMany({});
+        });
+    }
+    /**
      * Create subscription in DB
      */
     create() {
         return __awaiter(this, void 0, void 0, function* () {
             return db_connection_1.db.collection(enums_1.MongoCollection.subscriptions).insertOne(this);
+        });
+    }
+    /**
+     * Delete subscription from DB
+     */
+    delete() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Delete user from DB
+            return db_connection_1.db
+                .collection(enums_1.MongoCollection.subscriptions)
+                .deleteOne({ _id: { $eq: this._id } });
         });
     }
     /**
--- src/models/subscription.ts
+++ src/models/subscription.ts
@@ -1,15 +1,77 @@
-import { MongoCollection, UserRole } from "../utils/types/enums";
+import { MongoCollection } from "../utils/types/enums";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "../utils/database/db-connection";
-import {
-    DeleteWriteOpResultObject,
-    InsertOneWriteOpResult,
-    UpdateQuery,
-    UpdateWriteOpResult,
-} from "mongodb";
+import { DeleteWriteOpResultObject, InsertOneWriteOpResult } from "mongodb";
 import { CustomError } from "../utils/types/interfaces";
 
 export class Subscription {
+    /**
+     * Loads subscription from DB using its _id. The same can be achieved by passing all
+     * arguments to constructor
+     */
+    static async load(identifier: string): Promise<Subscription> {
+        // Using an _id
+        const subscriptionData = await db
+            .collection(MongoCollection.subscriptions)
+            .findOne({ _id: { $eq: identifier } });
+        // If a subscription is found, instanciate it and return the subscription
+        if (subscriptionData) {
+            return new this(subscriptionData);
+        } else {
+            // Throw not found error
+            const notFoundError: CustomError = {
+                statusCode: 404,
+                message: "Subscription not found",
+            };
+            throw notFoundError;
+        }
+    }
+
+    /**
+     * Loads all subscriptions from DB
+     */
+    static async loadAll(): Promise<Array<Subscription>> {
+        // Using and email
+        const subscriptionsData = await db
+            .collection(MongoCollection.subscriptions)
+            .find()
+            .toArray();
+        // If subscriptions are found, instanciate them and return the array
+        if (subscriptionsData.length > 0) {
+            const subscriptions = subscriptionsData.map(
+                (subcriptionData) => new this(subcriptionData)
+            );
+            return subscriptions;
+        } else {
+            // Throw not found error
+            const notFoundError: CustomError = {
+                statusCode: 404,
+                message: "No subscriptions found",
+            };
+            throw notFoundError;
+        }
+    }
+
+    /**
+     * Delete subscription from DB using its _id.
+     */
+    static async delete(
+        subscriptionId: string
+    ): Promise<DeleteWriteOpResultObject> {
+        // Delete user from DB
+        return db
+            .collection(MongoCollection.subscriptions)
+            .deleteOne({ _id: { $eq: subscriptionId } });
+    }
+
+    /**
+     * WARNING : Delete ALL user documents
+     */
+    static async deleteAll(): Promise<DeleteWriteOpResultObject> {
+        // Delete all user documents from DB
+        return db.collection(MongoCollection.subscriptions).deleteMany({});
+    }
+
     // Mandatory properties
     endpoint: string;
     keys: {
@@ -43,6 +105,16 @@ export class Subscription {
      */
     async create(): Promise<InsertOneWriteOpResult<any>> {
         return db.collection(MongoCollection.subscriptions).insertOne(this);
+    }
+
+    /**
+     * Delete subscription from DB
+     */
+    async delete(): Promise<DeleteWriteOpResultObject> {
+        // Delete user from DB
+        return db
+            .collection(MongoCollection.subscriptions)
+            .deleteOne({ _id: { $eq: this._id } });
     }
 
     /**
